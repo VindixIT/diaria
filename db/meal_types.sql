8f3dae54c380837dce5bd4a2033f5aa659e92da3
Masaru Ohashi Júnior <masaru@vindixit.com>
2020-08-20 19:40:29 -0300

    backup sql

--- db/meal_types.sql
+++ db/meal_types.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 11.1
 -- Dumped by pg_dump version 11.1
 
--- Started on 2020-08-20 19:36:06
+-- Started on 2020-08-20 19:35:26
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -40,16 +40,21 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 2833 (class 0 OID 692138)
--- Dependencies: 201
--- Data for Name: measures; Type: TABLE DATA; Schema: public; Owner: postgres
+-- TOC entry 2833 (class 0 OID 692132)
+-- Dependencies: 200
+-- Data for Name: meal_types; Type: TABLE DATA; Schema: public; Owner: vindixit
 --
 
-INSERT INTO public.measures VALUES (2, 'colher de sopa cheia');
-INSERT INTO public.measures VALUES (3, 'colher de chá');
+INSERT INTO public.meal_types VALUES (1, 'Café da Manhã', '06:00:00', '09:00:00');
+INSERT INTO public.meal_types VALUES (2, 'Almoço', '11:30:00', '14:00:00');
+INSERT INTO public.meal_types VALUES (3, 'Jantar', '17:00:00', '19:00:00');
+INSERT INTO public.meal_types VALUES (4, 'Lanche da Manhã', '09:00:00', '11:30:00');
+INSERT INTO public.meal_types VALUES (5, 'Lanche da Tarde', '14:00:00', '17:00:00');
+INSERT INTO public.meal_types VALUES (6, 'Ceia', '19:00:00', '21:00:00');
+INSERT INTO public.meal_types VALUES (7, 'Lanche Noturno', '21:00:00', '06:00:00');
 
 
--- Completed on 2020-08-20 19:36:06
+-- Completed on 2020-08-20 19:35:26
 
 --
 -- PostgreSQL database dump complete
